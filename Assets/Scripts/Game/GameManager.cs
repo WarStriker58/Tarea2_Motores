978d08eb991c86fb252f6042f41b571ab75fcb09
Sebastian Andre Salas Córdova <sebastian.salas@tecsup.edu.pe>
2024-05-05 21:49:53 -0500

    Distancia añadida

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -7,9 +7,11 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
     private int score = 0;
-    public Text scoreText;
+    private float distanceScore = 0f; // Puntuación basada en la distancia
+    public Text scoreText; // Referencia al objeto de texto para mostrar el puntaje
+    public Text distanceText; // Referencia al objeto de texto para mostrar la distancia
 
-    void Awake()
+    private void Awake()
     {
         if (instance == null)
         {
@@ -22,14 +24,30 @@ public class GameManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
+    private void Update()
+    {
+        // Actualiza la puntuación basada en la distancia
+        UpdateDistanceScore();
+        // Actualiza el texto del puntaje y la distancia
+        UpdateUIText();
+    }
+
     public void AddPoints(int pointsToAdd)
     {
         score += pointsToAdd;
-        UpdateScoreText();
+        UpdateUIText(); // Actualiza el texto del puntaje
     }
 
-    void UpdateScoreText()
+    private void UpdateDistanceScore()
     {
+        // Incrementa la puntuación basada en la distancia en función del tiempo transcurrido
+        distanceScore += Time.deltaTime * 30; // Ajusta el factor multiplicativo según tus necesidades
+    }
+
+    private void UpdateUIText()
+    {
+        // Actualiza el texto mostrando el puntaje y la distancia
         scoreText.text = "Score: " + score;
+        distanceText.text = "Distance: " + distanceScore.ToString("F0"); // Formatea la distancia como un entero
     }
 }
