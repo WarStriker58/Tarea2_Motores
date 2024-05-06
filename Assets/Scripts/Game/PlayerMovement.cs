cb95f43bf1843bfc438edd8201e86c2ffbcc6a17
Sebastian Andre Salas Córdova <sebastian.salas@tecsup.edu.pe>
2024-05-05 19:08:13 -0500

    Avance del movimiento del jugador 2

--- Assets/Scripts/Game/PlayerMovement.cs
+++ Assets/Scripts/Game/PlayerMovement.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -36,6 +36,45 @@ public class PlayerMovement : MonoBehaviour
             newPositionY = limitInferior;
         }
         transform.position = new Vector3(transform.position.x, newPositionY, transform.position.z);
+    }
+
+    void SetMinMax()
+    {
+        Vector3 bounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        limitInferior = -bounds.y;
+        limitSuperior = bounds.y;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Candy")
+        {
+            CandyGenerator.instance.ManageCandy(other.gameObject.GetComponent<CandyController>(), this);
+        }
+    }
+}*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D myRB;
+    [SerializeField] private float speed;
+    private float limitSuperior;
+    private float limitInferior;
+    public int player_lives = 3;
+
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+        SetMinMax();
+    }
+
+    void Update()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(mousePosition.y, limitInferior, limitSuperior), transform.position.z);
     }
 
     void SetMinMax()
